8655d7ffb9b546cf33c78645855d1e30d6d1c149
kkryl <k.krylov2005@gmail.com>
2024-02-26 12:11:27 +0400

    Final

---
+++ WpfApp1/Migrations/20240226054709_Initial.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Korzina",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User = table.Column<int>(type: "int", nullable: false),
+                    Nazv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kolvo = table.Column<int>(type: "int", nullable: false),
+                    Stoimost = table.Column<int>(type: "int", nullable: false),
+                    Pic = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Korzina", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tovar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nazv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kolvo = table.Column<int>(type: "int", nullable: false),
+                    Stoimost = table.Column<int>(type: "int", nullable: false),
+                    Pic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Opis = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tovar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Korzina");
+
+            migrationBuilder.DropTable(
+                name: "Tovar");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
